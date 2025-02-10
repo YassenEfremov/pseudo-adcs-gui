6fd8afdc3085d66351c5589742f1e0817611b75e
Yassen Efremov <yassen.efremov@gmail.com>
2025-02-10 16:03:58 +0200

    Add functions for connecting with bluetooth devices

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,64 +1,127 @@
-use btleplug::api::{Central, Manager as _, Peripheral as _, ScanFilter};
-use btleplug::platform::{Adapter, Manager, Peripheral};
+use btleplug::api::{BDAddr, Central, Characteristic, Manager as _, Peripheral as _, ScanFilter};
+use btleplug::platform::Peripheral;
 
+use tauri::{Manager, State};
+use tokio::sync::Mutex;
 use tokio::time::{sleep, Duration};
 
 
+#[derive(Default)]
+struct Settings {
+    devices: Vec<Peripheral>,
+    connected_device: Option<Peripheral>,
+}
+
 #[tauri::command]
-async fn ble() -> Result<Vec<String>, ()> {
-    let manager = Manager::new().await.unwrap();
+async fn scan(state: State<'_, Mutex<Settings>>) -> Result<Vec<(String, String)>, ()> {
+
+    let mut state = state.lock().await;
+
+    let manager = btleplug::platform::Manager::new().await.unwrap();
     let adapters = manager.adapters().await.unwrap();
     if adapters.is_empty() {
-        return Ok(vec![String::from("no adapters (BL is off or not supported)")]);
+        return Err(());
     }
-    // let mut adapters_info = Vec::<String>::new();
-    // for adapter in adapters.iter() {
-    //     adapters_info.push(adapter.adapter_info().await.unwrap());
-    // }
-    // Ok(adapters_info)
-
 
     let main_adapter = &adapters[0];
     main_adapter.start_scan(ScanFilter::default()).await;
 
+    // temporary
     sleep(Duration::from_secs(2)).await;
-    let peripherals = main_adapter.peripherals().await.unwrap();
-    if peripherals.is_empty() {
-        return Ok(vec![String::from("no peripherals")]);
+    state.devices = main_adapter.peripherals().await.unwrap();
+    if state.devices.is_empty() {
+        return Err(());
     }
     
-    let mut names = Vec::<String>::new();
-    for peripheral in peripherals.iter() {
+    let mut names_addresses = Vec::<(String, String)>::new();
+    for peripheral in state.devices.iter() {
         let props_opt = peripheral.properties().await.unwrap();
         if let Some(props) = props_opt {
-            names.push(props.local_name.unwrap_or(String::from("unknown")));
+            names_addresses.push(
+                (props.local_name.unwrap_or(String::from("unknown")),
+                props.address.to_string())
+            );
         }
     }
-    // peripherals[0].discover_services().await;
-    // let chars = peripherals[0].characteristics();
-    // if chars.is_empty() {
-    //     return Ok(vec![String::from("no chars")]);
-    // }
-    // let mut chars_str = Vec::<String>::new();
-    // for charec in chars.iter() {
-    //     chars_str.push(charec.to_string());
-    // }
-    // // chars.iter().find(|c| c.str)
-    
 
-    Ok(names)
+    Ok(names_addresses)
 }
 
 #[tauri::command]
-async fn pair() {
+async fn connect(state: State<'_, Mutex<Settings>>, addr_str: String) -> Result<(), ()> {
 
+    let mut settings = state.lock().await;
+
+    if let Some(connected_device) = &settings.connected_device {
+        // if connected_device.address().to_string() == addr_str {
+        //     return Err(());
+        // } else {
+        //     disconnect(state);
+        // }
+        return Err(());
+    }
+
+    let device = settings.devices.iter().find(|p|
+        p.address() == BDAddr::from_str_delim(&addr_str).unwrap()
+    ).unwrap();
+
+    if let Err(_) = device.connect().await {
+        return Err(());
+    }
+    settings.connected_device = Some(device.clone());   // not ideal :/
+    Ok(())
 }
+
+#[tauri::command]
+async fn disconnect(state: State<'_, Mutex<Settings>>) -> Result<(), ()> {
+
+    let mut settings = state.lock().await;
+
+    if let Some(connected_device) = &settings.connected_device {
+        if let Err(_) = connected_device.disconnect().await {
+            return Err(());
+        }
+        settings.connected_device = None;
+        Ok(())
+    } else {
+        Err(())
+    }
+}
+
+#[tauri::command]
+async fn telemetry(state: State<'_, Mutex<Settings>>) -> Result<String, ()> {
+
+    let mut settings = state.lock().await;
+
+    if let Some(connected_device) = &settings.connected_device {
+
+        connected_device.discover_services().await;
+        let chars = connected_device.characteristics();
+        
+        for characteristic in chars {
+            println!("{}", characteristic);
+        }
+        Ok("".into())
+    } else {
+        Err(())
+    }
+}
+
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![ble, pair])
+        .setup(|app| {
+            app.manage(Mutex::new(Settings::default()));
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![
+            scan,
+            connect,
+            disconnect,
+            telemetry,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -1,22 +1,59 @@
 const { invoke } = window.__TAURI__.core;
 
-let adapterListEl;
+let devicesEl;
+
+async function startMain() {
+  // await invoke("telemetry", {});
+}
+
+async function stopMain() {
+
+}
+
+async function toggleConnection(event) {
+  let deviceEl = event.target;
+  if (deviceEl.dataset.connected == "true") {
+    try {
+      await invoke("disconnect", {});
+      deviceEl.dataset.connected = "false";
+      deviceEl.style.backgroundColor = "white";
+      stopMain();
+    } catch (error) {
+      deviceEl.style.backgroundColor = "red";
+    }
+  } else {
+    try {
+      await invoke("connect", { addrStr: deviceEl.dataset.addrStr });
+      deviceEl.dataset.connected = "true";
+      deviceEl.style.backgroundColor = "lightblue";
+      startMain();
+    } catch (error) {
+      deviceEl.style.backgroundColor = "red";
+    }
+  }
+}
 
 async function scan() {
-  adapterListEl.innerHTML = "";
-  let adapterList = await invoke("ble", {});
-  for (const adapter of adapterList) {
-    const childEl = document.createElement("button");
-    childEl.textContent = adapter;
-    childEl.addEventListener("click", (e) => {
-      console.log("but");
-    });
-    adapterListEl.appendChild(childEl);
+  devicesEl.innerHTML = "";
+  try {
+    let devices = await invoke("scan", {});
+    for (const [name, addrStr] of devices) {
+      let deviceEl = document.createElement("button");
+      deviceEl.innerHTML = name;
+      deviceEl.dataset.addrStr = addrStr;
+      deviceEl.dataset.connected = "false";
+      deviceEl.addEventListener("click", toggleConnection);
+      devicesEl.appendChild(deviceEl);
+    }
+  } catch (error) {
+    let errEl = document.createElement("p");
+    errEl.innerHTML = "BL not enabled or not supported";
+    devicesEl.appendChild(errEl);
   }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  adapterListEl = document.querySelector("#devices");
+  devicesEl = document.querySelector("#devices");
   document.querySelector("#scan").addEventListener("click", (e) => {
     e.preventDefault();
     scan();
